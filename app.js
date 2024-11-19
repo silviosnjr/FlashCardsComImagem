b27dc2447937d72978bbe800095aa69fbbb7ab32
Silvio Sales do Nascimento Junior <silviosnjr@gmail.com>
2024-11-19 10:10:49 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+function criaCartao(categoria, pergunta, resposta, imagem){
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+
+    cartao.innerHTML = `
+    <div class="cartao__conteudo">
+    <h3>${categoria}</h3>
+    <div class="cartao__conteudo__pergunta">
+        <p>${pergunta}</p>
+        <img src="assets/img/${imagem}">
+    </div>
+    <div class="cartao__conteudo__resposta">
+        <p>${resposta}</p>
+    </div>
+    </div>
+    `
+    // código posterior ocultado
+
+    let respostaEstaVisivel = false
+
+    function viraCartao(){
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel);
+    }
+    cartao.addEventListener('click', viraCartao)
+    
+
+    container.appendChild(cartao)
+}
---
+++ perguntas.js
@@ -0,0 +1,27 @@
+criaCartao(
+    'Programação', 
+    'O que é Python?', 
+    'O Python é uma linguagem de programação',
+    'python.png'
+)
+
+criaCartao(
+    'Geografia', 
+    'Qual a capital da França?', 
+    'A capital da França é Paris',
+    'france.png'
+)
+
+criaCartao(
+    'Programação', 
+    'O que é uma função ?', 
+    'Uma função é um bloco de código que executa alguma tarefa',
+    'function.png'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em inglês',
+    "Oi em inglês é HI (RAI)",
+    'eng.png'
+)
